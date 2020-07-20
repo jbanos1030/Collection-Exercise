60196389fb20b24cf5d4a71fd374ff22a2defb1d
jbanos1030 <67976657+jbanos1030@users.noreply.github.com>
2020-07-20 13:38:07 -0500

    Add files via upload

---
+++ Jessica_B_CollectionExercise.playground/Contents.swift
@@ -0,0 +1,5 @@
+import UIKit
+
+var Souvenirs: [String] = [ "Boston, Massachusetts magnet"]
+Souvenirs += [ "New York City key chain", "New York City mug", "Las Vegas key chain", "Las Vegas Water bottle", "Six Flags Great America water bottle", "Disney Land 2019 landyard", "New Mexico mug", "Los Angeles, California key chain", "LA bracelet", "AL Capone Chicago key chain", "Boston cap", "LA shirt", "Mini statue of liberty" ]
+print(Souvenirs)
